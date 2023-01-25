5c30d461df459b5433cedfb75e2b298901094ea4
Dimon <bolilui.dmytro@gmail.com>
2023-01-25 17:12:34 +0200

    Add smooth scrolling effect

--- src/index.js
+++ src/index.js
@@ -64,6 +64,14 @@ function onLoadMore() {
     }
     appendArticlesMarkup(articles);
     lightbox.refresh();
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
   });
 }
 
